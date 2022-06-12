5dd178029d70d9336be17e89a63d8d66982585ea
stas <38563483+miarez@users.noreply.github.com>
2022-06-12 15:15:11 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
 
-include __DIR__."/../../sandbox-common.php";
-
-
-$esBuilderAutoLoad = function ($currentDir){
-    foreach(scandir($currentDir) as $dir)
-    {
-        if(preg_match("#^\.$|^\.\.$|\.php#", $dir)) continue;
-
-        $files = scandir($currentDir."/$dir/");
-
-        if($interfaceDirectoryIndex = array_search("_interfaces", $files)){
-            foreach(scandir($currentDir."/$dir/_interfaces/") as $interfaceFile){
-                if(preg_match("#^\.$|^\.\.$#", $interfaceFile)) continue;
-                include $currentDir."/$dir/_interfaces/$interfaceFile";
-            }
-            unset($files[$interfaceDirectoryIndex]);
-        }
-
-        foreach($files as $file)
-        {
-            if(preg_match("#^\.$|^\.\.$#", $file)) continue;
-            include $currentDir."/$dir/$file";
-        }
-    }
-};
-
-$esBuilderAutoLoad(__DIR__);
+include "core.php";
+esBuilderAutoLoad(__DIR__);
 
 
 $query = (new DSLQuery())->
@@ -41,19 +16,19 @@ $query = (new DSLQuery())->
             )->bindConditional(
                 (new MatchPhrase())
                     ->field("_event")
-                    ->query("jobredirect")
+                    ->query("click")
             )
             ->bindFilter(
                 (new Should())
                     ->bindConditional(
                         (new MatchPhrase())
                             ->field("host")
-                            ->query("www.talent.com")
+                            ->query("www.website.com")
                     )
                     ->bindConditional(
                         (new MatchPhrase())
                             ->field("host")
-                            ->query("uk.talent.com")
+                            ->query("uk.website.com")
                     )
             )
     )
@@ -63,8 +38,8 @@ $query = (new DSLQuery())->
             ->field("host")
             ->bindAggregationFunction(
                 (new Sum())
-                    ->field("billed_ppc")
-                    ->alias("billedTotal")
+                    ->field("ppc")
+                    ->alias("REVENUE")
             )
             ->bindFilter(
                 (new Must())
@@ -72,7 +47,7 @@ $query = (new DSLQuery())->
                     ->bindConditional(
                         (new Term())
                             ->field("host")
-                            ->termValue("www.talent.com")
+                            ->termValue("www.website.com")
                     )
                     ->bindAggregationFunction(
                         (new Count())
@@ -81,8 +56,8 @@ $query = (new DSLQuery())->
                     )
                     ->bindAggregationFunction(
                         (new Sum())
-                            ->field("billed_ppc")
-                            ->alias("billedFILTERED")
+                            ->field("ppc")
+                            ->alias("US_ONLY_REVENUE")
                     )
             )
 
@@ -91,7 +66,7 @@ $query = (new DSLQuery())->
     ->bindAggregationFunction(
         (new Count())
             ->field("_index")
-            ->alias("new_please_work")
+            ->alias("NON_NESTED_AGG_COUNT")
     )
 
     ->build()
@@ -99,175 +74,5 @@ $query = (new DSLQuery())->
 
 echo "<PRE>";
 echo json_encode($query, JSON_PRETTY_PRINT);
-echo "<BR>";
-echo "<BR>";
-echo "<BR>";
-echo "<BR>";
-print_r($query);
-
 exit;
 
-pp($query, 1);
-
-
-$query = '{
-    "from": 0,
-    "size": 0,
-    "query": {
-        "bool": {
-            "filter": [
-                {
-                    "bool": {
-                        "must": [
-                            {
-                                "range": {
-                                    "date": {
-                                        "from": "now\/M",
-                                        "to": null,
-                                        "include_lower": true,
-                                        "include_upper": true,
-                                        "boost": 1
-                                    }
-                                }
-                            },
-                            {
-                                "match_phrase": {
-                                    "_event": {
-                                        "query": "jobredirect",
-                                        "slop": 0,
-                                        "zero_terms_query": "NONE",
-                                        "boost": 1
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "billed_ppc": {
-                                        "from": "0",
-                                        "to": null,
-                                        "include_lower": false,
-                                        "include_upper": true,
-                                        "boost": 1
-                                    }
-                                }
-                            }
-                        ],
-                        "adjust_pure_negative": true,
-                        "boost": 1
-                    }
-                }
-            ],
-            "adjust_pure_negative": true,
-            "boost": 1
-        }
-    },
-    "_source": {
-        "includes": [
-            "COUNT"
-        ],
-        "excludes": []
-    },
-    "aggregations": {
-        "clicks": {
-            "value_count": {
-                "field": "_index"
-            }
-        }
-    }
-}';
-
-
-$query = '{
-	"from": 0,
-	"size": 0,
-	"query": {
-		"bool": {
-			"filter": [{
-				"bool": {
-					"must": [{
-						"bool": {
-							"must": [{
-								"range": {
-									"date": {
-										"from": "now\/M",
-										"to": null,
-										"include_lower": true,
-										"include_upper": true,
-										"boost": 1
-									}
-								}
-							}, {
-								"match_phrase": {
-									"_event": {
-										"query": "jobredirect",
-										"slop": 0,
-										"zero_terms_query": "NONE",
-										"boost": 1
-									}
-								}
-							}, {
-								"range": {
-									"billed_ppc": {
-										"from": "0",
-										"to": null,
-										"include_lower": false,
-										"include_upper": true,
-										"boost": 1
-									}
-								}
-							}],
-							"adjust_pure_negative": true,
-							"boost": 1
-						}
-					}],
-					"adjust_pure_negative": true,
-					"boost": 1
-				}
-			}],
-			"adjust_pure_negative": true,
-			"boost": 1
-		}
-	},
-	"_source": {
-		"includes": ["COUNT", "SUM", "SUM"],
-		"excludes": []
-	},
-	"aggregations": {
-		"host": {
-			"terms": {
-				"field": "host",
-				"execution_hint": "map",
-				"size": 1000,
-				"shard_size": 20000,
-				"min_doc_count": 1,
-				"shard_min_doc_count": 0,
-				"show_term_doc_count_error": false,
-				"order": [{
-					"_count": "desc"
-				}, {
-					"_key": "asc"
-				}]
-			},
-			"aggregations": {
-				"clicks": {
-					"value_count": {
-						"field": "_index"
-					}
-				},
-				"billedCAD": {
-					"sum": {
-						"script": {
-							"source": " def divide_578039473 = doc[\'billed_ppc_cad\'].value \/ 100;return divide_578039473;",
-							"lang": "painless"
-						}
-					}
-				},
-				"billed": {
-					"sum": {
-						"field": "billed_ppc"
-					}
-				}
-			}
-		}
-	}
-}';
